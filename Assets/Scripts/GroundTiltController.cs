64014cd1a51f0df2e4d2da1a57dd617e40ca2974
Maxime Philipp <maxime@studiopixmix.com>
2017-03-14 16:02:09 +0100

    Initial commit

---
+++ Assets/Scripts/GroundTiltController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTiltController : MonoBehaviour {
+
+	private const float MAX_GROUND_TILT_DEGREE = 0.30f;
+	private const int TILT_STEP = 15;
+	private const float ROTATION_SPEED = 80f;
+
+	// PROPERTIES :
+	private float tiltDirection;
+	private Quaternion turnRotation;
+	
+	// Update is called once per frame
+	private void Update () {
+		tiltDirection = -Input.GetAxis("Horizontal");
+	}
+
+	private void FixedUpdate() {
+		if(!tiltDirection.Equals(0f)) {
+			turnRotation = transform.rotation * Quaternion.Euler(0f, 0f, tiltDirection * ROTATION_SPEED * Time.deltaTime);
+
+			Debug.Log("Turn rotation = " + turnRotation.z);
+			turnRotation.z = Mathf.Clamp(turnRotation.z, -MAX_GROUND_TILT_DEGREE, MAX_GROUND_TILT_DEGREE);
+			Debug.Log("Clamp rotation = " + turnRotation.z);
+			transform.rotation = Quaternion.Lerp(transform.rotation, turnRotation, 40f * Time.deltaTime);
+
+			tiltDirection = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/JumpController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpController : MonoBehaviour {
+
+	private Vector2 jumpForce = new Vector2(0, 300f);
+
+	public float m_MinJumpForce = 10.0f;
+	public float m_MaxJumpForce = 10.0f;
+	public Vector2 m_MinJumpVector = new Vector2(-0.1f, 1.0f);
+	public Vector2 m_MaxJumpVector = new Vector2(0.1f, 1.0f);
+
+
+
+	// PROPERTIES :
+	private bool jumpRegistered;
+	private JellySprite jellySprite;
+
+	private void Start() {
+		jellySprite = GetComponent<JellySprite>();
+	}
+
+	// Update is called once per frame
+	private void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			jumpRegistered = true;
+			Debug.Log("Jump registered");
+		}
+	}
+
+	private void FixedUpdate() {
+		if(jumpRegistered) {
+			jellySprite.AddForce(jumpForce);
+
+			jumpRegistered = false;
+			Debug.Log("Performing jump.");
+		}
+	}
+}

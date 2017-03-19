c50f107667603ff2b48e3111220610cf8f7cef38
MaximePhilipp <philipp.maxime@gmail.com>
2017-03-20 00:37:22 +0100

    [WIP] Started adding the pnj emojis

---
+++ Assets/Scripts/Player/BubbleController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleController : MonoBehaviour {
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		Debug.Log("An emoji bubble has been popped. Freeing the emoji inside.");
+
+		JellySprite jellySprite = GetComponent<JellySprite>();
+		jellySprite.transform.parent = transform.parent;
+		jellySprite.m_GravityScale = 1.0f;
+
+		Destroy(this);
+	}
+}
